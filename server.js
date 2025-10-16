99804f1324253a1d3a676e5c235745a0a6166d4f
wazirwazir <151955963+wazirwazir@users.noreply.github.com>
2025-10-16 01:32:22 +0100

    more bugs

--- server.js
+++ server.js
@@ -7,11 +7,7 @@ const port = process.env.PORT || 3057;
 const app = express()
 
 
-app.use(cors({
-    origin: ['https://audiophile-ecommerce-api-1zr9.onrender.com/'],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-}))
+app.use(cors())
 app.use(express.json())
 
 
